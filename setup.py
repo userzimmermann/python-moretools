012ad0aa087ff79c6883f7e3280e3a061f3d7c6c
Stefan Zimmermann <zimmermann.code@googlemail.com>
2011-09-06 22:12:11 +0200

    better version number + missing quotes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
   name = 'python-moretools',
-  version = 0.0.1,
+  version = '0.1a',
   description = (
     'many more basic tools for python 2/3'
     + ' extending itertools, functools and operator'
