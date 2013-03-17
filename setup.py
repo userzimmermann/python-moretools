3e921db4e7fd7871c0e903d877fc2b688351678b
Stefan Zimmermann <zimmermann.code@gmail.com>
2013-03-17 19:50:27 +0100

    Version update (0.1a13)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name = 'python-moretools',
-  version = '0.1a12',
+  version = '0.1a13',
   description = (
     'many more basic tools for python 2/3'
     ' extending itertools, functools operator and collections'
