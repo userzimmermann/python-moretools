27de64e46ae0a3ed398d958320cbbbdeaf1ec2e3
Stefan Zimmermann <zimmermann.code@gmail.com>
2014-07-14 13:17:00 +0000

    setup: Call zetup().

--- setup.py
+++ setup.py
@@ -10,21 +10,7 @@ if 'sdist' in sys.argv:
         sys.exit(status)
 
 
-setup(
-  name=NAME,
-  version=VERSION,
-  description=(
-    'Many more basic tools for python 2/3'
-    ' extending itertools, functools, operator and collections.'
-    ),
-  author='Stefan Zimmermann',
-  author_email='zimmermann.code@gmail.com',
-  url='http://bitbucket.org/userzimmermann/python-moretools',
-
-  license='LGPLv3',
-
-  install_requires=REQUIRES,
-
+zetup(
   package_dir={
     'moretools.setup': '.',
     },
@@ -35,29 +21,4 @@ setup(
   package_data={
     'moretools.setup': SETUP_DATA,
     },
-
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved ::'
-    ' GNU Library or Lesser General Public License (LGPL)',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
-    'Topic :: Software Development',
-    'Topic :: Utilities',
-    ],
-  keywords=[
-    'tools', 'itertools', 'functools', 'operator', 'collections',
-    'iterator', 'iteration', 'functional',
-    'filter', 'map', 'repeat', 'query',
-    'tuple', 'list', 'set', 'dict', 'simpledict', 'frozen', 'struct',
-    'dictset', 'dictzip', 'simpledictset', 'simpledictzip',
-    'camelize', 'decamelize', 'isidentifier', 'identifier',
-    'python3',
-    ],
   )
